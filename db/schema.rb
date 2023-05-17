611716429357818e3812dd7ca61b3a4c1a9896f8
shunkatagiri <shun.katagiri1214@gmail.com>
2023-05-17 18:57:58 +0900

    テンプレートテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_15_215436) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_17_095511) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "templates", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "category"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_templates_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
@@ -24,4 +34,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_15_215436) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "templates", "users"
 end
