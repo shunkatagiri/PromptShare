7e49c1a2c1015fa0588ad4a1e2abf6d1fea01c41
shunkatagiri <shun.katagiri1214@gmail.com>
2023-05-17 18:57:05 +0900

    テンプレートテーブルのカラムを追加

--- db/migrate/20230517095511_create_templates.rb
+++ db/migrate/20230517095511_create_templates.rb
@@ -1,6 +1,10 @@
 class CreateTemplates < ActiveRecord::Migration[7.0]
   def change
     create_table :templates do |t|
+      t.string :title
+      t.text :description
+      t.string :category
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
