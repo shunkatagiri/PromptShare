16daf713097d09975fe73c5a90761065adb20dc1
shunkatagiri <shun.katagiri1214@gmail.com>
2023-05-21 18:49:13 +0900

    カテゴリのseedデータが追加されないように変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_18_083630) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_21_092302) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_18_083630) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id"
+    t.text "content"
     t.index ["user_id"], name: "index_templates_on_user_id"
   end
 
