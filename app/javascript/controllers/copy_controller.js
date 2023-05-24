6cc96e0b5eb7a4b392f6498a403dd935e23bfd52
shunkatagiri <shun.katagiri1214@gmail.com>
2023-05-24 23:55:52 +0900

    setTimeout関数を使ってコピーボタンの変化を元に戻すようにした

--- app/javascript/controllers/copy_controller.js
+++ app/javascript/controllers/copy_controller.js
@@ -21,9 +21,18 @@ export default class extends Controller {
   copySuccess() {
     this.buttonTextTarget.textContent = "Copied";
     this.iconTarget.innerHTML = `
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-5 h-5" data-copy-target="icon">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-5 h-5" data-copy-target="icon">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
+    </svg>
+  `;
+
+    setTimeout(() => {
+      this.buttonTextTarget.textContent = "Copy";
+      this.iconTarget.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-5 h-5">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"></path>
       </svg>
     `;
+    }, 2000);
   }
 }
