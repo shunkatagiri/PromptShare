6604972ecbde928bdd6b9c78ca318b13bcaf86e2
shunkatagiri <shun.katagiri1214@gmail.com>
2023-05-24 23:42:48 +0900

    コピーした後のUIの表示を変更

--- app/javascript/controllers/copy_controller.js
+++ app/javascript/controllers/copy_controller.js
@@ -1,17 +1,29 @@
+// copy_controller.js
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "source" ]
+  static targets = ["buttonText", "icon"]
 
   copy(event) {
-    event.preventDefault()
+    const source = this.data.get("source");
+    this.copyToClipboard(source);
+  }
 
-    navigator.clipboard.writeText(this.sourceTarget.textContent).then(() => {
-      // コピー成功時の処理
-      alert('Copied!');
-    }).catch((error) => {
-      // コピー失敗時の処理
-      console.error('コピー失敗：', error);
-    });
+  async copyToClipboard(text) {
+    try {
+      await navigator.clipboard.writeText(text);
+      this.copySuccess();
+    } catch (err) {
+      console.error('Failed to copy text: ', err);
+    }
+  }
+
+  copySuccess() {
+    this.buttonTextTarget.textContent = "Copied";
+    this.iconTarget.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-5 h-5" data-copy-target="icon">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
+      </svg>
+    `;
   }
 }
