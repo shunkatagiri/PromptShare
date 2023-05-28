ceac88b8ffe60abffc7cc5f0cc84d45c446ce108
shunkatagiri <shun.katagiri1214@gmail.com>
2023-05-28 22:36:58 +0900

    テンプレートがユーザーによってブックマークされているかを判定するメソッドを追加

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,7 +1,11 @@
 class Template < ApplicationRecord
   has_many :bookmarks
   has_many :bookmarking_users, through: :bookmarks, source: :user
-  
+
   belongs_to :user
   belongs_to :category
+
+  def bookmarked_by?(user)
+    bookmarks.where(user_id: user.id).exists?
+  end
 end
