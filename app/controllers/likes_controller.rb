50f12b868268c73ee91119f125c95f60ac6d325f
shunkatagiri <shun.katagiri1214@gmail.com>
2023-05-30 02:36:51 +0900

    likeコントローラーの修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,28 @@
+# app/controllers/likes_controller.rb
 class LikesController < ApplicationController
-  before_action :require_login, only: [:create, :destroy]
-  
+  before_action :set_template
+
   def create
-    @like = current_user.likes.create(template_id: params[:template_id])
-    redirect_to templates_path
+    @like = current_user.likes.build(template_id: params[:template_id])
+    @like.save
+
+    respond_to do |format|
+      format.turbo_stream
+    end
   end
 
   def destroy
-    @like = Like.find_by(user_id: current_user.id, template_id: params[:template_id])
+    @like = Like.find_by(template_id: params[:template_id], user_id: current_user.id)
     @like.destroy
-    redirect_to templates_path
+
+    respond_to do |format|
+      format.turbo_stream
+    end
+  end
+
+  private
+
+  def set_template
+    @template = Template.find(params[:template_id])
   end
 end
