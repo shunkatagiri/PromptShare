fe08f286470b51644399ab6df524b7e3cd5c8423
shunkatagiri <shun.katagiri1214@gmail.com>
2023-05-30 00:05:16 +0900

    likesコントローラーを作成し、アクションを定義

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,14 @@
+class LikesController < ApplicationController
+  before_action :authenticate_user!
+  
+  def create
+    @like = current_user.likes.create(template_id: params[:template_id])
+    redirect_to templates_path
+  end
+
+  def destroy
+    @like = Like.find_by(user_id: current_user.id, template_id: params[:template_id])
+    @like.destroy
+    redirect_to templates_path
+  end
+end
