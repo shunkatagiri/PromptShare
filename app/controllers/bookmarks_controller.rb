0f5ed3df5e29a586ac855dfd0bb6374fdc1115f5
shunkatagiri <shun.katagiri1214@gmail.com>
2023-05-29 16:06:18 +0900

    node_modulesをバージョン管理から除外

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,4 +1,5 @@
 class BookmarksController < ApplicationController
+  include ActionView::RecordIdentifier 
   before_action :set_template
   before_action :set_bookmark, only: :destroy
 
@@ -6,25 +7,18 @@ class BookmarksController < ApplicationController
     @bookmark = @template.bookmarks.new(user_id: current_user.id)
     if @bookmark.save
       respond_to do |format|
-        format.html do
-          flash[:notice] = 'ブックマークしました'
-          redirect_to @template
-        end
         format.turbo_stream { render turbo_stream: turbo_stream.replace(dom_id(@template, :bookmark), partial: 'bookmarks/unbookmark', locals: { bookmark: @bookmark }) }
       end
     else
-      flash[:notice] = '既にブックマークしています'
-      redirect_to @template
+      respond_to do |format|
+        format.turbo_stream { render turbo_stream: turbo_stream.replace(dom_id(@template, :bookmark), partial: 'bookmarks/bookmark', locals: { template: @template, error: '既にブックマークしています' }) }
+      end
     end
   end
 
   def destroy
     @bookmark.destroy
     respond_to do |format|
-      format.html do
-        flash[:notice] = 'ブックマークを解除しました'
-        redirect_to @template
-      end
       format.turbo_stream { render turbo_stream: turbo_stream.replace(dom_id(@template, :bookmark), partial: 'bookmarks/bookmark', locals: { template: @template }) }
     end
   end
@@ -36,5 +30,9 @@ class BookmarksController < ApplicationController
 
     def set_bookmark
       @bookmark = @template.bookmarks.find_by(user_id: current_user.id)
+      unless @bookmark
+        flash[:notice] = 'ブックマークが存在しません'
+        redirect_to @template
+      end
     end
 end
