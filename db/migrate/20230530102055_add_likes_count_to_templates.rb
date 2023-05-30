f2f50a1e027edfc6dbba0a7f1d699ad7b05ee119
shunkatagiri <shun.katagiri1214@gmail.com>
2023-05-30 19:39:25 +0900

    ビューを更新してソートオプションを追加

--- db/migrate/20230530102055_add_likes_count_to_templates.rb
+++ db/migrate/20230530102055_add_likes_count_to_templates.rb
@@ -1,5 +1,5 @@
-# db/migrate/*_add_likes_count_to_templates.rb
-
-def change
-  add_column :templates, :likes_count, :integer, default: 0, null: false
+class AddLikesCountToTemplates < ActiveRecord::Migration[6.1]
+  def change
+    add_column :templates, :likes_count, :integer, default: 0, null: false
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_145948) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_30_102055) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_145948) do
     t.text "content"
     t.text "usage_example"
     t.integer "importance"
+    t.integer "likes_count", default: 0, null: false
     t.index ["user_id"], name: "index_templates_on_user_id"
   end
 
