9bae8fbd2e112f52ce48a71e6a8ad7879a995c73
shunkatagiri <shun.katagiri1214@gmail.com>
2023-06-02 22:44:35 +0900

    ドロップダウンメニューを外側クリックで閉じる機能を追加

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -5,10 +5,25 @@ export default class extends Controller {
 
   connect() {
     console.log("Connected to dropdown menu controller.")
-    this.menuTarget.classList.add('hidden') // 追加
+    this.menuTarget.classList.add('hidden')
+    // ページ全体へのクリックイベントリスナを追加
+    document.addEventListener('click', this.outsideClick.bind(this))
   }
 
-  toggle() {
+  disconnect() {
+    // イベントリスナーを削除
+    document.removeEventListener('click', this.outsideClick.bind(this))
+  }
+
+  toggle(event) {
+    event.stopPropagation() 
     this.menuTarget.classList.toggle('hidden')
+  }
+
+  outsideClick(event) {
+    // メニューターゲットの外側がクリックされた場合、メニューを閉じる
+    if (!this.menuTarget.contains(event.target) && !this.menuTarget.classList.contains('hidden')) {
+      this.menuTarget.classList.add('hidden')
+    }
   }
 }
