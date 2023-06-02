811bc22f6bac3275d5cecc3f6ea52960ccf94c11
shunkatagiri <shun.katagiri1214@gmail.com>
2023-06-02 23:01:57 +0900

    ユーザーコントローラーにプロフィール編集機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:profile, :edit, :update]
+
   # User registration
   def new
     @user = User.new
@@ -16,11 +18,28 @@ class UsersController < ApplicationController
   end
 
   def profile
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "プロフィール情報が更新されました"
+      redirect_to user_profile_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
   end
 
   def user_params
-    params.require(:user).permit(:name, :email, :password ,:password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
   
 end
