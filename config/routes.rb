5b356a25c4a491c3e6f9c26f4ce8c3b181340af4
shunkatagiri <shun.katagiri1214@gmail.com>
2023-06-03 23:26:01 +0900

    ルーティングにtemplatesのsearchアクションのルートを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "tops#index"
+  
   resources :categories, only: :show do
     resources :templates, only: :index
   end
   
   resources :users, only: [:new, :create, :edit, :update]
+  
   resources :templates, only: [:index, :show, :create, :new, :destroy] do
     resources :bookmarks, only: [:create, :destroy, :index]
     resources :likes, only: [:create, :destroy] # 追加
+
+    collection do
+      match 'search' => 'templates#search', via: [:get, :post], as: :search
+    end
   end
+
   resources :bookmarks, only: [:index]
 
   get '/profile/:id', to: 'users#profile', as: :user_profile
