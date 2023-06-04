8d35506e638e538729b6fa2002a2fca79dd60cea
shunkatagiri <shun.katagiri1214@gmail.com>
2023-06-04 13:21:29 +0900

    【コミットメッセージ】 全てのコントローラーで検索オブジェクトを設定
    【詳細】
    ApplicationControllerにbefore_actionを追加し、全てのコントローラーで検索オブジェクトを設定するようにしました。これにより、各ビューで検索フォームを表示する際に必要な@qが常に利用可能になります。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
+# app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
+  before_action :set_search
+
+  private
+
+  def set_search
+    @q = Template.ransack(params[:q])
+  end
 end
