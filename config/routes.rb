c47ed0c6b06242022c387294ae0688f655d95e79
shunkatagiri <shun.katagiri1214@gmail.com>
2023-06-04 12:36:10 +0900

    ルーティングの修正: search_templates_pathの定義を追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,20 +6,16 @@ Rails.application.routes.draw do
   end
   
   resources :users, only: [:new, :create, :edit, :update]
-  
   resources :templates, only: [:index, :show, :create, :new, :destroy] do
     resources :bookmarks, only: [:create, :destroy, :index]
-    resources :likes, only: [:create, :destroy] # 追加
-
-    collection do
-      match 'search' => 'templates#search', via: [:get, :post], as: :search
-    end
+    resources :likes, only: [:create, :destroy]
   end
-
   resources :bookmarks, only: [:index]
 
   get '/profile/:id', to: 'users#profile', as: :user_profile
   get 'login', to: 'user_sessions#new', as: :login
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy', as: :logout
+
+  get 'search', to: 'templates#search', as: :search_templates
 end
