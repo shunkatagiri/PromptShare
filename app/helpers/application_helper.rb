f2bcd1900db87df042d6af1639e6b34e1296ba33
shunkatagiri <shun.katagiri1214@gmail.com>
2023-06-06 16:48:53 +0900

    Twitter共有用のヘルパーメソッドを作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
+# app/helpers/application_helper.rb
+
 module ApplicationHelper
+  def twitter_share_url(template)
+    title = template.title
+    url = template_url(template)
+    "https://twitter.com/share?url=#{url}&text=新しいテンプレートを投稿しました：#{title}"
+  end
 end
