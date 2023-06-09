4b124ade45753c17834b961b090f900ae79127c1
shunkatagiri <shun.katagiri1214@gmail.com>
2023-06-09 22:05:39 +0900

    編集・更新機能をログイン済みユーザーのみに制限

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,6 +1,6 @@
 # app/controllers/templates_controller.rb
 class TemplatesController < ApplicationController
-  before_action :require_login, only: [:create]
+  before_action :require_login, only: [:create ,:edit, :update]
 
   def index
     @category = Category.find(params[:category_id]) if params[:category_id].present?
