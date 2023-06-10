2d9bee97eafecd1a4c7c673d013fb441be42fa45
shunkatagiri <shun.katagiri1214@gmail.com>
2023-06-10 18:06:50 +0900

    Userモデルのファクトリを作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    name { "テストユーザー" }
+    email { "test@example.com" }
+    crypted_password { "password" }
+    salt { "salt" }
+    introduction { "こんにちは、テストユーザーです。" }
   end
 end
