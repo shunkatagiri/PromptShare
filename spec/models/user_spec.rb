b2670b36b0daba1df92d4d054584ef789ca369a2
shunkatagiri <shun.katagiri1214@gmail.com>
2023-06-10 17:14:22 +0900

    userモデルのrspecテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # ユーザーを新しく作成するためのメソッド
+  def build_user(attrs = {})
+    described_class.new({
+      name: "John Doe",
+      email: "john.doe@example.com",
+      password: "password",
+      password_confirmation: "password",
+    }.merge(attrs))
+  end
+
+  describe 'associations' do
+    it 'has many bookmarks' do
+      should have_many(:bookmarks)
+    end
+
+    it 'has many bookmarked_templates through bookmarks' do
+      should have_many(:bookmarked_templates).through(:bookmarks)
+    end
+
+    it 'has many likes' do
+      should have_many(:likes)
+    end
+
+    it 'has many liked_templates through likes' do
+      should have_many(:liked_templates).through(:likes)
+    end
+
+    it 'has many templates' do
+      should have_many(:templates)
+    end
+  end
+
+  describe 'validations' do
+    it 'requires a name to be present' do
+      user = build_user(name: nil)
+      expect(user.valid?).to eq(false)
+    end
+
+    it 'requires a unique email' do
+      user1 = build_user
+      user2 = build_user
+      user1.save!
+      expect(user2.valid?).to eq(false)
+    end
+
+    it 'requires a password to be present on create' do
+      user = build_user(password: nil, password_confirmation: nil)
+      expect(user.valid?).to eq(false)
+    end
+
+    it 'requires a password to be confirmed on create' do
+      user = build_user(password_confirmation: "not_password")
+      expect(user.valid?).to eq(false)
+    end
+
+    it 'requires a password to be at least 3 characters long' do
+      user = build_user(password: 'ab', password_confirmation: 'ab')
+      expect(user.valid?).to eq(false)
+    end
+  end
 end
