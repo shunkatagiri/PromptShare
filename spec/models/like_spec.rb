1c8093514100bb8cf54c8f51597c56ce6479d84b
shunkatagiri <shun.katagiri1214@gmail.com>
2023-06-10 18:30:23 +0900

    Likeモデルのアソシエーションテストを追加

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it 'belongs to user' do
+      is_expected.to belong_to(:user)
+    end
+
+    it 'belongs to template' do
+      is_expected.to belong_to(:template)
+    end
+  end
 end
