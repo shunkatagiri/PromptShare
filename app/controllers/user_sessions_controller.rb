1719f59a394810ddefb0c08e735555b63927ea13
shunkatagiri <shun.katagiri1214@gmail.com>
2023-06-11 16:07:13 +0900

    認証失敗時にTurboストリームのレスポンスをレンダリングするように変更

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,13 +4,16 @@ class UserSessionsController < ApplicationController
 
   def create
     @user = User.find_by(email: params[:email])
-  
+
     if @user&.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to root_path
     else
       flash.now[:alert] = 'メールアドレスまたはパスワードが間違っています。'
-      render :new
+      respond_to do |format|
+        format.turbo_stream { render turbo_stream: turbo_stream.replace('user_sessions_new', partial: 'user_sessions/new') }
+        format.html { render :new }
+      end
     end
   end
 
