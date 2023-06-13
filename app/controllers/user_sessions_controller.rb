2aa346ae29af0823a4c6695313789d1b0becd404
shunkatagiri <shun.katagiri1214@gmail.com>
2023-06-13 20:11:49 +0900

    ユーザー認証の際のエラーを解消

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -5,7 +5,7 @@ class UserSessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:email])
 
-    if @user&.authenticate(params[:password])
+    if @user
       session[:user_id] = @user.id
       redirect_to root_path
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
   has_many :bookmarked_templates, through: :bookmarks, source: :template
   has_many :likes
   has_many :liked_templates, through: :likes, source: :template
-  
+  has_secure_password validations: false
+
   validates :name, presence: true
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, presence: true, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
