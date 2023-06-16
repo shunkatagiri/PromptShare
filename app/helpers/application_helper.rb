7ff813e31645f78ddc66ecb1e31d5c39339e061a
shunkatagiri <shun.katagiri1214@gmail.com>
2023-06-16 11:28:24 +0900

    Twitter共有リンク生成のためのヘルパーメソッドを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def twitter_share_url(options = {})
-    tweet_text = ERB::Util.url_encode(options[:title])
+    tweet_text = ERB::Util.url_encode("Check out this template: #{options[:title]}")
     tweet_url = ERB::Util.url_encode(options[:url])
     "https://twitter.com/share?url=#{tweet_url}&text=#{tweet_text}"
   end
