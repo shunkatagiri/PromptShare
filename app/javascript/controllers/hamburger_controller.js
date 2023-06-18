d2db5d9cc72cc9468073c251793cc9ac40bfe2cc
shunkatagiri <shun.katagiri1214@gmail.com>
2023-06-18 15:19:31 +0900

    ステップ2: メニューの表示/非表示を切り替える機能をコントローラに追加

---
+++ app/javascript/controllers/hamburger_controller.js
@@ -0,0 +1,14 @@
+// app/javascript/controllers/hamburger_controller.js
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "menu" ]
+
+  connect() {
+    this.menuTarget.classList.add('hidden')
+  }
+
+  toggle() {
+    this.menuTarget.classList.toggle('hidden')
+  }
+}
