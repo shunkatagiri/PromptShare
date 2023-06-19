a59786d6fbf498baf2f9e49463b46e7d05d51b44
shunkatagiri <shun.katagiri1214@gmail.com>
2023-06-19 21:04:11 +0900

    未ログイン時のリダイレクトを設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 # app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
   before_action :set_search
+  
 
   private
+
+  def redirect_to_landing_page_if_not_logged_in
+    redirect_to root_path unless logged_in?
+  end
 
   def set_search
     @q = Template.ransack(params[:q])
