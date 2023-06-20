c27e7522a5c5d03736eaaddbdb596860a7afc8b8
shunkatagiri <shun.katagiri1214@gmail.com>
2023-06-20 16:24:28 +0900

    未ログイン時のリダイレクト先をトップページからログインページへ変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,15 @@
 # app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
+  before_action :require_login
   before_action :set_search
   
 
   private
 
   def redirect_to_landing_page_if_not_logged_in
-    redirect_to root_path unless logged_in?
+    redirect_to login_url unless logged_in?
   end
-
+  
   def set_search
     @q = Template.ransack(params[:q])
   end
