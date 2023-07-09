c9b80a89f9addf75ed401adf7a911820e3cc8628
shunkatagiri <shun.katagiri1214@gmail.com>
2023-07-09 21:09:45 +0900

    acts-as-taggable-on gemを追加してインストール

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_17_072426) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_09_120509) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,37 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_17_072426) do
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
+  create_table "taggings", force: :cascade do |t|
+    t.bigint "tag_id"
+    t.string "taggable_type"
+    t.bigint "taggable_id"
+    t.string "tagger_type"
+    t.bigint "tagger_id"
+    t.string "context", limit: 128
+    t.datetime "created_at", precision: nil
+    t.string "tenant", limit: 128
+    t.index ["context"], name: "index_taggings_on_context"
+    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+    t.index ["taggable_id", "taggable_type", "context"], name: "taggings_taggable_context_idx"
+    t.index ["taggable_id", "taggable_type", "tagger_id", "context"], name: "taggings_idy"
+    t.index ["taggable_id"], name: "index_taggings_on_taggable_id"
+    t.index ["taggable_type", "taggable_id"], name: "index_taggings_on_taggable_type_and_taggable_id"
+    t.index ["taggable_type"], name: "index_taggings_on_taggable_type"
+    t.index ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type"
+    t.index ["tagger_id"], name: "index_taggings_on_tagger_id"
+    t.index ["tagger_type", "tagger_id"], name: "index_taggings_on_tagger_type_and_tagger_id"
+    t.index ["tenant"], name: "index_taggings_on_tenant"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "taggings_count", default: 0
+    t.index ["name"], name: "index_tags_on_name", unique: true
+  end
+
   create_table "templates", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -63,6 +94,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_17_072426) do
     t.text "chat_content"
     t.string "link"
     t.index ["user_id"], name: "index_templates_on_user_id"
+  end
+
+  create_table "user_preferences", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.text "preferences", default: "{}", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_preferences_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -81,5 +120,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_17_072426) do
   add_foreign_key "feedbacks", "users"
   add_foreign_key "likes", "templates"
   add_foreign_key "likes", "users"
+  add_foreign_key "taggings", "tags"
   add_foreign_key "templates", "users"
+  add_foreign_key "user_preferences", "users"
 end
